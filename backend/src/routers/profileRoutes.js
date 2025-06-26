0da86cc813e482f9e9c6bedc33c760bf3ed95a49
William2152 <133088365+William2152@users.noreply.github.com>
2025-06-26 23:18:31 +0700

    Add User Description Editable

--- backend/src/routers/profileRoutes.js
+++ backend/src/routers/profileRoutes.js
@@ -4,5 +4,6 @@ const profileController = require("../controllers/ProfileController");
 const router = Router();
 
 router.get("/getprofile/:firebaseId", profileController.getProfile);
+router.patch("/updateMyProfile/:firebaseId", profileController.updateMyProfile);
 
 module.exports = router;
